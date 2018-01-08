cdd2ee2772be67140998898255b6ca350af057e4
Edilberto Costa Neto <edilcn@gmail.com>
2018-01-08 19:22:18 -0200

    Envio de JSON com Tag+Timestamp. Apenas lê tag e envia para o HassIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,132 @@
 #include <Homie.h>
+#include <ArduinoJson.h>
+#include <NtpClientLib.h>
+#include <MFRC522.h>
+#include <WiFiUDP.h>
+#include <TimeLib.h>
+
+// Variáveis RFID
+const int rfidss = 15;
+const int rfidgain = 112;
+int PIN_RELAY = 4;
+unsigned long cooldown = 0;
+
+// Variáveis NTP
+const char * ntpserver = "br.pool.ntp.org";
+int ntpinter = 30;
+WiFiUDP ntpUDP;
+int16_t timeZone = -3;
+uint32_t currentMillis = 0;
+uint32_t previousMillis = 0;
+// Create MFRC522 RFID instance
+MFRC522 mfrc522 = MFRC522();
+
+// Funções RFID
+void setupRFID(int rfidss, int rfidgain);
+
+void ShowReaderDetails() {
+  // Get the MFRC522 software version
+  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+  Serial.print(F("[ INFO ] MFRC522 Version: 0x"));
+  Serial.print(v, HEX);
+  if (v == 0x91)
+    Serial.print(F(" = v1.0"));
+  else if (v == 0x92)
+    Serial.print(F(" = v2.0"));
+  else if (v == 0x88)
+    Serial.print(F(" = clone"));
+  else
+    Serial.print(F(" (unknown)"));
+  Serial.println("");
+  // When 0x00 or 0xFF is returned, communication probably failed
+  if ((v == 0x00) || (v == 0xFF)) {
+    Serial.println(F("[ WARN ] Communication failure, check if MFRC522 properly connected"));
+  }
+}
+void setupRFID(int rfidss, int rfidgain) {
+  SPI.begin();           // MFRC522 Hardware uses SPI protocol
+  mfrc522.PCD_Init(rfidss, UINT8_MAX);    // Initialize MFRC522 Hardware
+  // Set RFID Hardware Antenna Gain
+  // This may not work with some boards
+  mfrc522.PCD_SetAntennaGain(rfidgain);
+  Serial.printf("[ INFO ] RFID SS_PIN: %u and Gain Factor: %u", rfidss, rfidgain);
+  Serial.println("");
+  ShowReaderDetails(); // Show details of PCD - MFRC522 Card Reader details
+}
+
+/*--------------------Funções e declarações para o HOMIE----------------------*/
+HomieNode rfidNode("reader", "Rfid");
+HomieNode lockNode("lock", "Relay");
+
+bool lockOnHandler(const HomieRange& range, const String& value) {
+  if (value != "true" && value != "false") return false;
+
+  bool on = (value == "true");
+  digitalWrite(PIN_RELAY, !on ? HIGH : LOW);
+  rfidNode.setProperty("open").send(value);
+  Homie.getLogger() << "Autorização: " << (on ? "true" : "false") << endl;
+
+  return true;
+}
+
+void setupHandler() {
+  rfidNode.setProperty("read").send("c");
+}
+
+void loopHandler() {
+  //If a new PICC placed to RFID reader continue
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    delay(50);
+    return;
+  }
+  //Since a PICC placed get Serial (UID) and continue
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    delay(50);
+    return;
+  }
+  // We got UID tell PICC to stop responding
+  mfrc522.PICC_HaltA();
+  cooldown = millis() + 2000;
+
+  // There are Mifare PICCs which have 4 byte or 7 byte UID
+  // Get PICC's UID and store on a variable
+  Serial.print(F("[ INFO ] PICC's UID: "));
+  String uid = "";
+  for (int i = 0; i < mfrc522.uid.size; ++i) {
+    uid += String(mfrc522.uid.uidByte[i], HEX);
+  }
+  // Get PICC type
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  String type = mfrc522.PICC_GetTypeName(piccType);
+
+  // Prepara o envio do JSON para o Servidor
+  StaticJsonBuffer<300> JSONbuffer;
+  JsonObject& JSONencoder = JSONbuffer.createObject();
+  JSONencoder["tag"] = uid;
+  JSONencoder["time"] = NTP.getTime();
+  char JSONmessageBuffer[300];
+  JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  Homie.getLogger() << "Leitura da TAG: " << JSONmessageBuffer << endl;
+  rfidNode.setProperty("read").send(JSONmessageBuffer); // Define o tópico soho/deviceID/lockreader/read como publish
+}
+////////////////////////////////////////////////////////////////////////////////
+//1533133620        1533130020
 
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial << endl << endl;
+    pinMode(PIN_RELAY, OUTPUT);
+    digitalWrite(PIN_RELAY, HIGH);
+    Homie_setFirmware("RFID Reader", "0.0.1");
+    setupRFID(rfidss, rfidgain);
+    NTP.begin(ntpserver, timeZone);
+    NTP.setInterval(ntpinter * 60);
+    Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+    lockNode.advertise("open").settable(lockOnHandler);
+    rfidNode.advertise("read");
+    Homie.setup();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    Homie.loop();
 }
